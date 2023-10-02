b901d3fa9887da093026fe7ea9acf15ab42f6c5e
Aranzazu-Barrutia <aranzazufinos@gmail.com>
2023-10-02 13:49:53 +0200

    changed structure and finnished

--- src/js/main.js
+++ src/js/main.js
@@ -9,87 +9,118 @@ const btnReset = document.querySelector('.js-button-reset');
 //const icon = document.querySelector('.js-delete-icon');
 
 //Array series
-let searchSeries = [];
-let favSeries = new Array();
+let listSeries = []; // array objeto serie
+let favSeries = []; //lista de fav
 //funciones
-function getApiData() {
-  const value = inputSearch.value;
-  const url = `//api.tvmaze.com/search/shows?q=${value}`;
-  fetch(url)
+// esta funcion hace que se queden las series seleccionadas al refrescar la pagina
+function getItem() {
+  const storedFavorites = localStorage.getItem('favorites');
+
+  if (storedFavorites) {
+    favSeries = JSON.parse(storedFavorites);
+
+    // Muestro los elementos favoritos en la lista de favoritos (favList)
+    favList.innerHTML = '';
+
+    for (const favorite of favSeries) {
+      const id = favorite.show.id;
+      const name = favorite.show.name;
+      const alternativeImageUrl =
+        'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+      const imageUrl =
+        favorite.show.image === null
+          ? alternativeImageUrl
+          : favorite.show.image.medium;
+
+      favList.innerHTML += `<li id=${id} class="div_list_section_ul_card js-serie">
+        <img class="div_list_section_ul_img" src="${imageUrl}" alt="">
+        <h2>${name}</h2>
+        <button class="btnX js-button-delete" onclick="">X</button>
+      </li>`;
+    }
+  }
+}
+
+// Llamo a la función getItem al cargar la página para cargar los favoritos almacenados.
+getItem();
+
+function getApiData(ev) {
+  ev.preventDefault(); //prevengo q el formulario se evie al hacer clic
+
+  formList.innerHTML = ''; // borro el contenido dentro de la lista
+  const value = inputSearch.value; //obtengo el valor de la usuaria
+  const url = `https://api.tvmaze.com/search/shows?q=${value}`; //concatenamos el valor de la API y el de la usuaria
+  fetch(url) //solicito a la URL
     .then((response) => {
-      return response.json();
+      return response.json(); //recogemos la solicitud y la pasamos a string
     })
     .then((dataAPI) => {
-      // eslint-disable-next-line no-console
-      console.log('DataAPI -> ', dataAPI);
-      searchSeries = dataAPI;
-      renderSerieList(dataAPI);
-      renderFavSerieList(favSeries);
-    });
-  //.catch((e) => console.log('Error -> ', e));
-}
+      //ejecuto el código y lo paso como argumento
 
-//Datos de una serie, generamos el HTML para  luego ver la serie en una lista con su nombre e imagen.
-function renderSerie(oneSerie) {
-  const id = oneSerie.show.id;
-  const name = oneSerie.show.name;
-  const alternativeImageUrl =
-    'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-  const imageUrl =
-    oneSerie.show.image === null
-      ? alternativeImageUrl
-      : oneSerie.show.image.medium;
-  return `<li  id=${id} class="div_list_section_ul_card js-serie"><img class="div_list_section_ul_img" src="${imageUrl}" alt=""><h2>${name}</h2><button class="btnX js-button-delete" onclick="">X</button></li>`;
+      listSeries = dataAPI; //lista de series obtenida de la API
+      for (const serie of dataAPI) {
+        //recorremos la lista
+        //console.log(serie);
+        const id = serie.show.id; //extraigo las propiedades de los elementos
+        const name = serie.show.name;
+        const alternativeImageUrl =
+          'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        const imageUrl =
+          serie.show.image === null
+            ? alternativeImageUrl
+            : serie.show.image.medium;
+        formList.innerHTML += `<li  id=${id} class="div_list_section_ul_card js-serie">
+        <img class="div_list_section_ul_img" src="${imageUrl}" alt="">
+        <h2>${name}</h2>
+        <button class="btnX js-button-delete">X</button>
+        </li>`; //introduzco los elementos en el HTML
+      }
+
+      addEventToSerie(); //agrego evento clic a cada elemento de la serie
+    });
 }
-//muestro el listado de series,genero el HTML y
-//cada serie se genera usando la función renderSerie
-function renderSerieList(listSeries) {
-  let html = '<ul>';
-  for (const oneSerie of listSeries) {
-    html += renderSerie(oneSerie);
-  }
-  html += '</ul>';
-  formList.innerHTML = html;
-  addEventToSerie();
-}
-//muestro lalista de favoritos en el HTML, recorro el array de series y omito las series que aparcen como undefined.
-function renderFavSerieList(listSeries) {
-  let html = '<ul>';
-  for (const oneSerie of listSeries) {
-    if (oneSerie === undefined) {
-      continue;
-    }
-    html += renderSerie(oneSerie);
-  }
-  html += '</ul>';
-  favList.innerHTML = html;
-}
-//funcion manejadora xa el ev click de una serie, encunetro la serie en la lista de search series,
-//la pongo en favoritas y se muestra a la usuaria la lista de fav actualizada.
-function handleClick(event) {
-  event.preventDefault();
-  const idSerieClick = parseInt(event.currentTarget.id);
-  const favSerie = searchSeries.find(
+function favorites(event) {
+  //manejo el evento de hacer clic xa maracarla como favorita
+  event.preventDefault(); //prevengo q el formulario se evie al hacer clic
+  const idSerieClick = parseInt(event.currentTarget.id); //lo busco por nº y no x string y obtengo el ID de laa serie clicada
+  const selectedSerie = listSeries.find(
+    //busco la serie en ek array usando el ID
     (oneSerie) => oneSerie.show.id === idSerieClick
   );
-  if (favSeries.indexOf(favSerie) === -1) {
-    favSeries.push(favSerie);
+
+  if (selectedSerie) {
+    //verifico si se encuentro la serie con el ID
+    favSeries.push(selectedSerie);
+    event.currentTarget.classList.add('favSeries'); //serie marcada favorita
+    event.currentTarget.classList.remove('oneSerie');
+
+    const id = selectedSerie.show.id;
+    const name = selectedSerie.show.name;
+    const alternativeImageUrl =
+      'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+    const imageUrl =
+      selectedSerie.show.image === null
+        ? alternativeImageUrl
+        : selectedSerie.show.image.medium;
+
+    favList.innerHTML += `<li id=${id} class="div_list_section_ul_card js-serie">
+        <img class="div_list_section_ul_img" src="${imageUrl}" alt="">
+        <h2>${name}</h2>
+        <button class="btnX js-button-delete" onclick="">X</button>
+      </li>`; ////introduzco los elementos en el HTML
+
+    localStorage.setItem('favorites', JSON.stringify(favSeries));
   }
-  event.currentTarget.classList.add('oneSerie');
-  event.currentTarget.classList.add('favSerie');
-  renderFavSerieList(favSeries);
-  localStorage.setItem('favorites', JSON.stringify(favSerie));
 }
 
-//aagrego  el click a cada elemento serie para que la usuaria pueda agregarla a la lista favoritas.
+// //muestro lalista de favoritos en el HTML, recorro el array de series y omito las series que aparcen
 function addEventToSerie() {
   const allSeries = document.querySelectorAll('.js-serie');
-  for (const oneSerie of allSeries) {
-    oneSerie.addEventListener('click', handleClick);
+  for (const serie of allSeries) {
+    serie.addEventListener('click', favorites);
   }
 }
 
-//function handleDelete() {}
 function handleReset() {
   const fav = document.querySelector('.js-section-fav');
   favSeries = [];
@@ -98,13 +129,7 @@ function handleReset() {
   localStorage.setItem('myShows', JSON.stringify(favList));
 }
 btnReset.addEventListener('click', handleReset);
-
-function updateFavList() {
-  renderFavSerieList(favSeries);
-}
-updateFavList();
-getApiData();
 //evento click sobre boton -> ejecuta handle
-btnSearch.addEventListener('click', handleClick);
+btnSearch.addEventListener('click', getApiData);
 
 //btnX.addEventListener('click', handleDelete);
